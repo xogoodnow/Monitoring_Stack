0940a57749322cfffa05578331cd219442961bd5
xogoodnow <xo4ever9999@gmail.com>
2023-12-21 14:28:07 +0330

    Added terraform to the project

---
+++ Terraform/main.tf
@@ -0,0 +1,22 @@
+module "ssh_keys" {
+  source = "./modules/ssh_keys"
+  hcloud_token = var.hcloud_token
+}
+
+
+module "servers" {
+  source       = "./modules/servers"
+  hcloud_token = var.hcloud_token
+  image_name   = var.image_name
+  server_type  = var.server_type
+  location     = var.location
+  depends_on   = [module.ssh_keys]
+}
+
+
+
+module "monitoring" {
+  source = "./modules/Monitoring"
+  hcloud_token = var.hcloud_token
+  depends_on = [module.volume]
+}
